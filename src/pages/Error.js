c3cafcad5d2b57ee20e6afd4a766aa9430b05f03
JamesAlexandre <alexandrejames82000@gmail.com>
2022-10-11 20:03:51 +0200

    Ajout page 404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,10 +1,20 @@
+import { NavLink } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import ErrorCSS from "../style/Error.module.css";
 
 const Error = () => {
-    return (
-        <div>
-            <h1>404</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <div className={ErrorCSS.container}>
+        <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <NavLink to="/">Retourner sur la page d'accueil</NavLink>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Error;
